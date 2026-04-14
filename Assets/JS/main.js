0d1bad1c2b2c6ca54026cf1b8a38b5aad9987999
Huy <Huyanh052222@gmail.com>
2026-04-14 14:36:44 +0700

    update khung suon

---
+++ Assets/JS/main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    function loadComponent(id, path) {
+        fetch(path)
+            .then(response => {
+                if (!response.ok) throw new Error("Không tìm thấy file: " + path);
+                return response.text();
+            })
+            .then(data => {
+                document.getElementById(id).innerHTML = data;
+            })
+            .catch(error => console.error(error));
+    }
+
+    loadComponent("header-placeholder", "../Components/header.html");
+});
